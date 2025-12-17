5072e0453886b37394a6ae3503ea7dad104500ec
Novoselov Stepan <novosel.stepan@gmail.com>
2025-12-17 14:20:13 +0700

    feat: add CRUD endpoints for emission source

--- EcoPulseBackend/Controllers/EmissionSourceController.cs
+++ EcoPulseBackend/Controllers/EmissionSourceController.cs
@@ -1,4 +1,5 @@
 ﻿using EcoPulseBackend.Contexts;
+using EcoPulseBackend.Models.EmissionSource;
 using Microsoft.AspNetCore.Mvc;
 
 namespace EcoPulseBackend.Controllers;
@@ -13,11 +14,73 @@ public class EmissionSourceController : ControllerBase
         _dbContext = dbContext;
     }
 
+    [HttpPost("/emissionSource")]
+    public async Task<IActionResult> AddEmissionSource([FromBody] EmissionSourceAddModel model)
+    {
+        var emissionSource = new EmissionSource
+        {
+            Lon = model.Lon,
+            Lat = model.Lat,
+            EjectedTemp = model.EjectedTemp,
+            AvgExitSpeed = model.AvgExitSpeed,
+            HeightSource = model.HeightSource,
+            DiameterSource = model.DiameterSource,
+            TempStratificationRatio = model.TempStratificationRatio,
+            SedimentationRateRatio = model.SedimentationRateRatio
+        };
+        
+        _dbContext.EmissionSources.Add(emissionSource);
+        await _dbContext.SaveChangesAsync();
+        
+        return Ok(emissionSource);
+    }
+
     [HttpGet("/emissionSource")]
-    public IActionResult GetEmissionSourceList()
+    public IActionResult GetAllEmissionSources()
     {
         var result = _dbContext.EmissionSources.ToList();
 
         return Ok(result);
     } 
+    
+    [HttpPut("/emissionSource")]
+    public async Task<IActionResult> UpdateEmissionSource([FromBody] EmissionSourceUpdateModel model)
+    {
+        var emissionSource = _dbContext.EmissionSources.FirstOrDefault(s => s.Id == model.Id);
+
+        if (emissionSource == null)
+        {
+            return NotFound();
+        }
+        
+        emissionSource.Lon = model.Lon;
+        emissionSource.Lat = model.Lat;
+        emissionSource.EjectedTemp = model.EjectedTemp;
+        emissionSource.AvgExitSpeed = model.AvgExitSpeed;
+        emissionSource.HeightSource = model.HeightSource;
+        emissionSource.DiameterSource = model.DiameterSource;
+        emissionSource.TempStratificationRatio = model.TempStratificationRatio;
+        emissionSource.SedimentationRateRatio = model.SedimentationRateRatio;
+        
+        _dbContext.EmissionSources.Update(emissionSource);
+        await _dbContext.SaveChangesAsync();
+        
+        return Ok(emissionSource);
+    }
+    
+    [HttpDelete("/emissionSource/{id:int}")]
+    public async Task<IActionResult> DeleteEmissionSource(int id)
+    {
+        var emissionSource = _dbContext.EmissionSources.FirstOrDefault(s => s.Id == id);
+
+        if (emissionSource == null)
+        {
+            return NotFound();
+        }
+        
+        _dbContext.EmissionSources.Remove(emissionSource);
+        await _dbContext.SaveChangesAsync();
+        
+        return Ok(emissionSource);
+    }
 }
---
+++ EcoPulseBackend/Models/EmissionSource/EmissionSourceUpdateModel.cs
@@ -0,0 +1,59 @@
+﻿using System.ComponentModel.DataAnnotations;
+using EcoPulseBackend.Enums;
+
+namespace EcoPulseBackend.Models.EmissionSource;
+
+/// <summary>
+/// Модель изменения источника выброса
+/// </summary>
+public class EmissionSourceUpdateModel
+{
+    /// <summary>
+    /// Идентификатор
+    /// </summary>
+    public int Id { get; set; }
+    
+    /// <summary>
+    /// Долгота
+    /// </summary>
+    public float Lon { get; set; }
+    
+    /// <summary>
+    /// Широта
+    /// </summary>
+    public float Lat { get; set; }
+    
+    /// <summary>
+    /// Температура выбрасываемой ГВС
+    /// </summary>
+    [Range(235, 265)]
+    public float EjectedTemp { get; set; }
+
+    /// <summary>
+    /// Средняя скорость выхода ГВС из устья источника выброса, м/с
+    /// </summary>
+    [Range(15, 30)]
+    public float AvgExitSpeed { get; set; }
+
+    /// <summary>
+    /// Высота источника выброса, м.
+    /// </summary>
+    [Range(13, 150)]
+    public float HeightSource { get; set; }
+
+    /// <summary>
+    /// Диаметр устья источника, м.
+    /// </summary>
+    [Range(1, 10)]
+    public float DiameterSource { get; set; }
+
+    /// <summary>
+    /// Коэффициент региона
+    /// </summary>
+    public CoefficientRegion TempStratificationRatio { get; set; }
+
+    /// <summary>
+    /// Коэффициент степени очистки
+    /// </summary>
+    public CoefficientDegreePurification SedimentationRateRatio { get; set; }
+}
