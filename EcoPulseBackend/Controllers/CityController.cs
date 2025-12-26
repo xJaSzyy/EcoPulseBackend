f97c794b8c403e6a80e3b517277d1659a45c6569
Novoselov Stepan <novosel.stepan@gmail.com>
2025-12-26 15:50:34 +0700

    feat: add city controller

---
+++ EcoPulseBackend/Controllers/CityController.cs
@@ -0,0 +1,28 @@
+﻿using EcoPulseBackend.Contexts;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EcoPulseBackend.Controllers;
+
+[ApiController]
+public class CityController : ControllerBase
+{
+    private readonly ApplicationDbContext _dbContext;
+
+    public CityController(ApplicationDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    [HttpGet("city/{id:int}")]
+    public IActionResult GetCityById(int id)
+    {
+        var city = _dbContext.Cities.FirstOrDefault(c => c.Id == id);
+
+        if (city == null)
+        {
+            return NotFound();
+        }
+
+        return Ok(city);
+    }
+}
