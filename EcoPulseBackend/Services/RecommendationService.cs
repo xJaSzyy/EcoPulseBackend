e082868ea42cf045315188eadcc4cb8371272a0c
xjasz <novoselov.s@v2grp.ru>
2026-02-03 10:12:37 +0700

    feat: add ollama service and system prompt

--- EcoPulseBackend/Services/RecommendationService.cs
+++ EcoPulseBackend/Services/RecommendationService.cs
@@ -9,7 +9,7 @@ namespace EcoPulseBackend.Services;
 public class RecommendationService : IRecommendationService
 {
     private readonly string _ollamaUrl;
-    private const string ModelName = "llama3.2";
+    private const string ModelName = "gpt-oss:120b-cloud";
     
     public RecommendationService(IConfiguration configuration)
     {
@@ -20,7 +20,21 @@ public class RecommendationService : IRecommendationService
     {
         var result = new RecommendationResult();
 
-        var systemPrompt = "Ты - помощник по рекомендациям в сфере экологии.";
+        var systemPrompt = "Ты - специалист по рекомендациям в сфере экологии. " +
+                           "Твоя задача четко и кратко формулировать советы и рекомендации для жителей города. " +
+                           "Ты должен выделить 3 рекомендации, не больше, не меньше. " +
+                           "Эти рекомендации должны быть сформулированы четко и ясно, максимально кратко, но со смыслом." +
+                           "Советы должны быть для обычных жителей города." +
+                           "Никаких форматирований (без жирного текста)" +
+                           "Как могут выглядеть рекомендации: " +
+                           "1. Желательно сократить время пребывания на улице" +
+                           "2. По возможности, не открывайте сегодня окна" +
+                           "3. Больше выходите на прогулки - воздух чистый" +
+                           "4. Наслаждайтесь активным отдыхом на улице без каких-либо опасений за здоровье." +
+                           "5. Качество воздуха приемлемо, но чувствительные люди могут испытывать легкий дискомфорт." +
+                           "6. Ограничьте активный отдых на улице." +
+                           "7. Избегайте активного отдыха на улице." +
+                           "8. Пользуйтесь общественным транспортом вместо личных автомобилей, чтобы уменьшить общие выбросы в атмосферу.";
 
         using var httpClient = new HttpClient();
         httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
